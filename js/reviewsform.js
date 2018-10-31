83761200e1264395e459d5f4ec018ec4949b558e
Shadrach Temitayo <shadrachtemitayo@gmail.com>
2018-10-31 22:41:52 +0100

    fix: Trap Tabkey to manage focus
    - Add keydown listener to form when reviews modal is being displayed
    - If tab | tab + shift key is pressed manage focus in the form
    - if `esc` key is pressed remove modal
    
    [Ticket: X]

--- js/reviewsform.js
+++ js/reviewsform.js
@@ -14,7 +14,6 @@ class ReviewsForm {
     
     // form animation keys
     this.formAnimationKeys = [
-      {display: 'none', transform: 'scale(.2)', opacity: '0'},
       {display: 'grid', transform: 'scale(.5)', opacity: '0'},
       {display: 'grid', transform: 'scale(1)', opacity: '1', easing: 'cubic-bezier(.35,.97,.13,1.14)'}
     ];
@@ -22,7 +21,7 @@ class ReviewsForm {
     // create form animation and pause it
     this.formAnimation = this.form.animate(
       this.formAnimationKeys,
-      {duration: 300}
+      {duration: 200}
     );
     this.formAnimation.pause();
     this.setListener(this.addReviewButton, 'click', this.showReviewForm);
@@ -59,6 +58,38 @@ class ReviewsForm {
     this.setListener(this.submitBtn, 'submit', this.submitReview);
     this.setListener(this.closeBtn, 'click', this.hideReviewForm);
     this.setListener(this.formOverlay, 'click', this.hideReviewForm);
+    
+    // this.setListener(this.form, 'keydown', this.trapTabKey);
+    this.form.addEventListener('keydown', (e) => {
+      this.trapTabKey(e);
+    }, false);
+  }
+
+  trapTabKey(evt) {
+    const focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
+    const focusableElements = [...this.form.querySelectorAll(focusableElementsString)];
+    const firstTabStop = focusableElements[0];
+    const lastTabStop = focusableElements[focusableElements.length - 1];
+    
+    // TAB
+    if(evt.keyCode === 9) {
+      
+      // SHIFT + TAB
+      if(evt.shiftKey) {
+        if(document.activeElement === firstTabStop) {
+          lastTabStop.focus();
+        }
+      } else {
+        if(document.activeElement === lastTabStop) {
+          firstTabStop.focus();
+        }
+      }
+      
+    }
+    
+    if(evt.keyCode === 27) {
+      this.hideReviewForm();
+    }
   }
   
   removeListeners() {
