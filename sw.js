f0c39899ec8d98f3391811756f717e2c3d059c01
Shadrach Temitayo <shadrachtemitayo@gmail.com>
2018-11-07 12:58:34 +0100

    fix: multiple image caches
    - merge staticFilesCache and imageCache into one

--- sw.js
+++ sw.js
@@ -1,35 +1,5 @@
 const staticFileCache = 'v1';
-const imageCache = 'v2';
-const staticCacheNames = [staticFileCache, imageCache];
-
-const imagesToCache = [
-	'./img/1.jpg',
-	'./img/2.jpg',
-	'./img/3.jpg',
-	'./img/4.jpg',
-	'./img/5.jpg',
-	'./img/6.jpg',
-	'./img/7.jpg',
-	'./img/8.jpg',
-	'./img/9.jpg',
-	'./img/10.jpg',
-	'./img/1_small.jpg',
-	'./img/2_small.jpg',
-	'./img/3_small.jpg',
-	'./img/4_small.jpg',
-	'./img/5_small.jpg',
-	'./img/6_small.jpg',
-	'./img/7_small.jpg',
-	'./img/8_small.jpg',
-	'./img/9_small.jpg',
-	'./img/10_small.jpg',
-	'./img/neighbourhood.svg',
-	'./img/cuisine.svg',
-	'./img/restaurant-icons_192x192.png',
-	'./img/restaurant-icons_256x256.png',
-	'./img/restaurant-icons_512x512.png',
-	'./favicon.png'
-]
+const staticCacheNames = [staticFileCache];
 
 const filesToCache = [
 	'/',
@@ -42,6 +12,13 @@ const filesToCache = [
 	'./js/restaurant_info.js',
 	'./js/reviewsform.js',
 	'/js/idb.js',
+	'./img/placeholder.jpg',
+	'./img/neighbourhood.svg',
+	'./img/cuisine.svg',
+	'./img/restaurant-icons_192x192.png',
+	'./img/restaurant-icons_256x256.png',
+	'./img/restaurant-icons_512x512.png',
+	'./favicon.png',
 	'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
 	'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
 ];
@@ -94,22 +71,16 @@ self.addEventListener('fetch', event => {
 });
 
 function preCache() {
-	caches.open(staticFileCache).then(cache => {
+	return caches.open(staticFileCache).then(cache => {
 		cache.addAll(filesToCache)
 			.catch(err => console.log(err, 'static assets failed to be cached'))
 	});
-
-	return caches.open(imageCache).then(cache => {
-		cache.addAll(imagesToCache)
-		.then(() => console.log('static images successfully cached'))
-		.catch((e) => console.warn('static images could not be cached', e))
-	})
 }
 
 
 serveImages = (request) => {
 	let networkFetch = fetch(request);
-	return caches.open(imagesToCache).then(cache => {
+	return caches.open(staticFileCache).then(cache => {
 		return cache.match(request).then(response => {
 			return response || networkFetch.then(networkResponse => {
 				if(networkResponse)
