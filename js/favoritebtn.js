fb2d4084b1cf3aff20be1dbcb7acbcd61e422998
Shadrach Temitayo <shadrachtemitayo@gmail.com>
2019-01-15 16:33:14 +0100

    fix: schedule dom updates using raf

--- js/favoritebtn.js
+++ js/favoritebtn.js
@@ -2,21 +2,31 @@ class FavoriteBtn {
   constructor(el, restaurant) {
     this.restaurant = restaurant;
     this.el = el;
-    this.el.setAttribute('role', 'switch')
+    this.el.setAttribute("role", "switch");
     this.isFavorite = this.restaurant.is_favorite;
     this.render();
   }
 
   render() {
-    if(this.isFavorite) {
-      this.el.classList.add('is-favorite');
+    if (this.isFavorite) {
+      this.el.classList.add("is-favorite");
     } else {
-      this.el.classList.remove('is-favorite');
+      this.el.classList.remove("is-favorite");
     }
 
-    this.el.setAttribute('title', this.isFavorite ? `unfavorite ${this.restaurant.name}` : `favorite ${this.restaurant.name}`);
-    this.el.setAttribute('aria-checked', this.isFavorite);
-    this.el.setAttribute('aria-label', this.isFavorite ? `unfavorite ${this.restaurant.name}` : `favorite ${this.restaurant.name}`);
+    this.el.setAttribute(
+      "title",
+      this.isFavorite
+        ? `unfavorite ${this.restaurant.name}`
+        : `favorite ${this.restaurant.name}`
+    );
+    this.el.setAttribute("aria-checked", this.isFavorite);
+    this.el.setAttribute(
+      "aria-label",
+      this.isFavorite
+        ? `unfavorite ${this.restaurant.name}`
+        : `favorite ${this.restaurant.name}`
+    );
     this.setClick(this.el, this.updateFavorite.bind(this));
   }
 
@@ -29,9 +39,9 @@ class FavoriteBtn {
   }
 
   updateFavorite() {
-    this.toggleFavorite(this.isFavorite); 
+    this.toggleFavorite(this.isFavorite);
     this.restaurant.is_favorite = this.isFavorite;
-    this.render();
+    requestAnimationFrame(this.render);
     mainhelper.updateRestaurant(this.restaurant);
     RestaurantFetch.setFavourite(this.restaurant.id, this.isFavorite);
   }
--- js/restaurantfetch.js
+++ js/restaurantfetch.js
@@ -3,7 +3,7 @@
  */
 class RestaurantFetch {
   static get DATABASE_URL() {
-    const port = 1337 // Change this to your server port ---http://localhost:${port}
+    const port = 1337; // Change this to your server port ---http://localhost:${port}
     return `http://localhost:${port}`;
   }
 
@@ -12,7 +12,7 @@ class RestaurantFetch {
   }
 
   static RESTAURANT_URL(id) {
-    return `${this.DATABASE_URL}/restaurants/${id}`
+    return `${this.DATABASE_URL}/restaurants/${id}`;
   }
 
   static get REVIEWS_URL() {
@@ -25,57 +25,49 @@ class RestaurantFetch {
 
   static FAVORITES_URL(id, value) {
     return `http://localhost:1337/restaurants/${id}/?is_favorite=${value}`;
-
   }
 
   static fetchRestaurants() {
-    return fetch(this.RESTAURANTS_URL)
-            .then(response => response.json())
+    return fetch(this.RESTAURANTS_URL).then(response => response.json());
   }
 
   static fetchRestaurant(id = 0) {
-    return fetch(this.RESTAURANT_URL(id))
-      .then(response => response.json());
+    return fetch(this.RESTAURANT_URL(id)).then(response => response.json());
   }
 
   static fetchAllReviews() {
-    return fetch(this.REVIEWS_URL)
+    return fetch(this.REVIEWS_URL);
   }
 
   static fetchReviews(id) {
-    return fetch(this.RESTAURANT_REVIEWS_URL(id))
-      .then(response => response.json());
+    return fetch(this.RESTAURANT_REVIEWS_URL(id)).then(response =>
+      response.json()
+    );
   }
 
   static fetchReview(id) {
-    return fetch(this.REVIEW_URL(id))
-      .then(response => response.json());
+    return fetch(this.REVIEW_URL(id)).then(response => response.json());
   }
 
   static createReview(review) {
-    return fetch(this.REVIEWS_URL,
-      {
-        method: 'POST',
-        body: JSON.stringify(review),
-        headers: {
-          "Content-Type": "application/json; charset=utf-8"
-        }
-      }).then(response => response.json())
+    return fetch(this.REVIEWS_URL, {
+      method: "POST",
+      body: JSON.stringify(review),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      }
+    }).then(response => response.json());
   }
 
   static createReviews(reviews) {
-    return Promise.all(
-      reviews.map(review => this.createReview(review))
-    ).then(responses => {
-      return responses;
-    })
+    return Promise.all(reviews.map(review => this.createReview(review))).then(
+      responses => responses
+    );
   }
 
   static setFavourite(id, value) {
-    return fetch(this.FAVORITES_URL(id, value),
-      {
-        method: 'PUT'
-      }
-    ).then(res => console.log(res));
+    return fetch(this.FAVORITES_URL(id, value), {
+      method: "PUT"
+    }).then(res => console.log(res));
   }
 }
